6b9d12b4542bc60dbdd6886ef8b99f2d6a73ef6e
Anastasia Hanna Ougolnikova <95757425+nxstix@users.noreply.github.com>
2023-06-29 08:38:16 +0200

    Update next.config.js

--- nextjs-blog/next.config.js
+++ nextjs-blog/next.config.js
@@ -12,6 +12,16 @@ const nextConfig = {
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
+      },
+      {
+        // matching all routes inside the 'pages' folder
+        source: "/pages/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
       }
     ]
   }
