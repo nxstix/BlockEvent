d4d12f528c0f67fa20d04f26c867f384e3b4e57b
orlapin <104128954+orlapin@users.noreply.github.com>
2023-06-29 09:46:24 +0200

    Update index.js

--- nextjs-blog/pages/index.js
+++ nextjs-blog/pages/index.js
@@ -25,10 +25,10 @@ export default function Home({ events, sortList }) {
             Buy, trade and store safely in your wallet
           </h5>
         </div>
-        // <div tyle={{ marginLeft: "2rem" }}>
-        //   <h4><b style={{ marginLeft: "2rem" }}>Coming Up:</b></h4>
-        //   <EventList events={sortList} />
-        // </div>
+        <div tyle={{ marginLeft: "2rem" }}>
+          <h4><b style={{ marginLeft: "2rem" }}>Coming Up:</b></h4>
+          <EventList events={sortList} />
+        </div>
         <div style={{ marginTop: "1rem" }}>
           <h4><b style={{ marginLeft: "2rem" }}>Popular:</b></h4><br></br>
           <EventList events={events} />
@@ -40,18 +40,18 @@ export default function Home({ events, sortList }) {
 
 export async function getServerSideProps() {
   try {
-    const response = await fetch("http://localhost:3000/api/events");
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/events`);
     if (!response.ok) {
       throw new Error("Failed to fetch events");
     }
     const events = await response.json();
-    // let sortList = [...events]; // Create a copy of the events array
+    let sortList = [...events]; // Create a copy of the events array
 
-    // sortList = sortList.sort((a, b) => new Date(a.dateString) - new Date(b.dateString));
+    sortList = sortList.sort((a, b) => new Date(a.dateString) - new Date(b.dateString));
     return {
       props: {
         events,
-        // sortList
+        sortList
       },
     };
   } catch (error) {
@@ -59,7 +59,7 @@ export async function getServerSideProps() {
     return {
       props: {
         events: null,
-        // sortList
+        sortList
       },
     };
   }
